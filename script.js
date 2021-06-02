ebfffca429efd85ac7ec250b6b9896ef539ea6c0
Mauro <maurogheco@gmail.com>
2021-06-01 22:31:20 -0300

    added additional button functionality, enter tasks by enter key, and fixed selecting ul issue

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const toDoingBtn = document.querySelector('.to-doing');
 const toDoneBtn = document.querySelector('.to-done');
 const tasksTodo = document.querySelector('.tasks__todo');
 const tasksDoing = document.querySelector('.tasks__doing');
+const tasksDone = document.querySelector('.tasks__done');
 const emptyErr = document.querySelector('.emptyErr');
 
 let idTask = 0;
@@ -18,7 +19,6 @@ const addTask = () => {
     li.appendChild(document.createTextNode(valueInputTask));
     li.id = `task-${idTask}`;
     li.className = 'task-item';
-
     tasksTodo.appendChild(li);
     idTask++;
     inputTask.value = '';
@@ -27,20 +27,41 @@ const addTask = () => {
 
 const selectTask = (event) => {
   // console.log(event.target);
-  event.target.classList.toggle('selected-task');
+  if (event.target.classList[0] === 'task-item') {
+    event.target.classList.toggle('selected-task');
+  }
 };
 
 const moveToDoing = () => {
-  let tasksList = document.querySelectorAll('.selected-task');
+  let tasksList = document.querySelectorAll('.tasks__todo .selected-task');
   console.log(tasksList[0]);
   while (tasksList.length > 0) {
     tasksTodo.removeChild(tasksList[0]);
     tasksDoing.appendChild(tasksList[0]);
     tasksList[0].classList.remove('selected-task');
-    tasksList = document.querySelectorAll('.selected-task');
+    tasksList = document.querySelectorAll('.tasks__todo .selected-task');
+  }
+};
+
+const moveToDone = () => {
+  let tasksList = document.querySelectorAll('.tasks__doing .selected-task');
+  console.log(tasksList[0]);
+  while (tasksList.length > 0) {
+    tasksDoing.removeChild(tasksList[0]);
+    tasksDone.appendChild(tasksList[0]);
+    tasksList[0].classList.remove('selected-task');
+    tasksList = document.querySelectorAll('.tasks__doing .selected-task');
   }
 };
 
 addTaskBtn.addEventListener('click', addTask);
+inputTask.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    addTask();
+  }
+});
 tasksTodo.addEventListener('click', selectTask);
+tasksDoing.addEventListener('click', selectTask);
 toDoingBtn.addEventListener('click', moveToDoing);
+toDoneBtn.addEventListener('click', moveToDone);
